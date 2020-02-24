3547f60ddc3c4573f13def31fc7e4643c8d5ec8d
m.kodina <m.kodina@corp.vk.com>
2020-02-24 12:36:20 +0300

    refactoring some test

--- lesson_13/browserstack/test_browserstack.py
+++ lesson_13/browserstack/test_browserstack.py
@@ -1,6 +1,26 @@
 """File runs tests on the cloud grid - browserstack"""
-
+import pytest
 from selenium import webdriver
+
+
+@pytest.fixture()
+def driver(request):
+    wd = webdriver.Remote(
+        command_executor='http://marina433:FLYaq7T4RLnD9Ts75pCv@hub.browserstack.com:80/wd/hub',
+        desired_capabilities=desired_cap)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_google(driver):
+    driver.get("http://www.google.com/ncr")
+    if "Google" not in driver.title:
+        raise Exception("Unable to load google page!")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys("BrowserStack")
+    elem.submit()
+    print(driver.title)
+
 
 desired_cap = {
     'browser': 'Chrome',
@@ -10,16 +30,3 @@ desired_cap = {
     'resolution': '1024x768',
     'name': 'Bstack-[Python] Sample Test'
 }
-
-driver = webdriver.Remote(
-    command_executor='http://marina433:FLYaq7T4RLnD9Ts75pCv@hub.browserstack.com:80/wd/hub',
-    desired_capabilities=desired_cap)
-
-driver.get("http://www.google.com/ncr")
-if "Google" not in driver.title:
-    raise Exception("Unable to load google page!")
-elem = driver.find_element_by_name("q")
-elem.send_keys("BrowserStack")
-elem.submit()
-print(driver.title)
-driver.quit()
